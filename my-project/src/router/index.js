734dfbf7438d8f2153facf27c5bce421876f3386
peirong-js <56344165+peirong-js@users.noreply.github.com>
2020-01-31 13:18:44 +0800

    first vue pages
    first vue pages demo1

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld' //路径，@代表src目录
+import demo1 from '@/pages/demo1'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/',	//页面地址栏路径
       name: 'HelloWorld',
-      component: HelloWorld
+      component: HelloWorld   //页面名称，通过上面的import导入
+    },
+    {
+      path: '/demo1',
+      name: 'demo1',
+      component: demo1
     }
   ]
 })
