cf799ee30e4690b3a50596c3fb06c94dd6622046
peirong-js <56344165+peirong-js@users.noreply.github.com>
2020-02-08 14:51:51 +0800

    add style
    add style

--- my-project/src/pages/demo2/index.vue
+++ my-project/src/pages/demo2/index.vue
@@ -2,13 +2,13 @@
 	<div>
 		<div>选项/数据</div>
 		<div>----------------------------------------------</div>
-		<div style="font-size: 12px;color:gray">data: 全局组件变量定义，定义的变量可以渲染到模板上（数据绑定），用{{}}输出</div>
+		<div class="describe">data: 全局组件变量定义，定义的变量可以渲染到模板上（数据绑定），用{{}}输出</div>
 		<div>data: {{msg}}</div>
 		<div>----------------------------------------------</div>
-		<div style="font-size: 12px;color:gray">computed: 模板中可以绑定函数，定义到computed中</div>
+		<div class="describe">computed: 模板中可以绑定函数，定义到computed中</div>
 		<div>computed: {{aDouble}}</div>
 		<div>----------------------------------------------</div>
-		<div style="font-size: 12px;color:gray">methods: 绑定事件，触发函数</div>
+		<div class="describe">methods: 绑定事件，触发函数</div>
 		<div @click="say('hi')">methods:点击我</div>
 	</div>
 </template>
@@ -35,3 +35,10 @@
 	    }
 	}
 </script>
+
+<style type="text/css">
+	.describe{
+    font-size: 12px;
+    color:gray;
+  }
+</style>
