9dc8b9616968342bf5fb1d41a9f4a409fefdafeb
peirong-js <56344165+peirong-js@users.noreply.github.com>
2020-02-10 10:57:43 +0800

    自定义组件
    - 组件写到components文件夹下
    - 组件基本要素：props（接收属性值） 、$emit（自定义触发事件）等
    - 通过import将组件引入到页面中
    - 通过components注册组件

---
+++ my-project/src/components/countdown.vue
@@ -0,0 +1,32 @@
+<template>
+	<p :style="{color: col}">{{time}}</p>
+</template>
+
+
+<script>
+	export default{
+		data(){
+			return{
+				time: 10
+			}
+		},
+		mounted(){
+			var vm = this;
+			var t = setInterval(function(){
+				vm.time--;
+				if(vm.time == 0){					
+					clearInterval(t);
+					vm.$emit("end");
+				}
+			},1000)
+		},
+
+		//接收传过来的属性值col，通过:style="{color: col}" 应用到P标签中
+		props:{
+			col:{
+				type: String,
+				default: '#000'
+			}
+		}
+	}
+</script>
---
+++ my-project/src/pages/demo10/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<div>自定义组件</div>
+		<div>--------------------------------</div>
+		<div class="describe">
+			- 组件写到components文件夹下<br>
+			- 组件基本要素：props 、$emit等<br>
+			- 通过import将组件引入到页面中<br>
+			- 通过components注册组件<br>
+		</div>
+		<!-- 标签的形式引入 -->
+		<count-down col="blue" @end="ending"></count-down>
+	</div>
+</template>
+
+
+<script>
+	//import引入组件
+	import countdown from '@/components/countdown.vue'
+
+	export default{
+		data(){
+			return{
+
+			}
+		},
+		methods:{
+			ending(){
+				alert("已经结束了")
+			}
+		},
+		//注册
+		components:{
+			'count-down':countdown
+		}
+	}
+</script>
+
+<style type="text/css">
+	.describe{
+    font-size: 12px;
+    color:gray;
+  }
+</style>
